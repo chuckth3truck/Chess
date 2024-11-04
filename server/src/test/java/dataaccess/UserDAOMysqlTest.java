22986e57c42f29892b332b67f8fc2752eada483d
Charles Oliphant <coli2001@byu.edu>
2024-11-03 22:01:56 -0700

    created UserDAOMysql tests and they all pass

--- server/src/test/java/dataaccess/UserDAOMysqlTest.java
+++ server/src/test/java/dataaccess/UserDAOMysqlTest.java
@@ -1,24 +1,61 @@
 package dataaccess;
+import model.userData;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserDAOMysqlTest {
+    private userDataAccess userMemory;
 
-    @Test
-    void getUserData() {
+    public UserDAOMysqlTest() {
+
+        try {
+            userMemory = new UserDAOMysql();
+            System.out.println("using mysql user");
+        } catch (Exception e) {
+            userMemory = new userDAOMemory();
+        }
     }
 
+
+
     @Test
+    @Order(1)
     void addUser() {
+        userData user = new userData("user","pass", "email@email");
+        assertDoesNotThrow(() -> userMemory.addUser(user));
     }
 
     @Test
-    void clear() {
+    @Order(2)
+    void getUserData() {
+        try {
+            assertDoesNotThrow(() -> userMemory.getUserData("user"));
+            assertNotEquals(null, userMemory.getUserData("user"));
+            assertEquals(null, userMemory.getUserData("blue"));
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
+
     @Test
+    @Order(3)
     void checkUserExists() {
+        assertEquals(true, userMemory.checkUserExists("user"));
+        assertEquals(false, userMemory.checkUserExists("blue"));
+    }
+
+    @Test
+    @Order(4)
+    void clear() {
+        assertDoesNotThrow(() -> userMemory.clear());
     }
 }
