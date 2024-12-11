edce880886998907b92b10fdc58e67e43ebeae9a
Charles Oliphant <coli2001@byu.edu>
2024-12-11 11:10:34 -0700

    made some more changes

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,9 +3,23 @@ import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var server = new Server();
-        server.run(8080);
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        try {
+            var port = 8080;
+            if (args.length >= 1) {
+                port = Integer.parseInt(args[0]);
+            }
+
+            new Server().run(port);
+            System.out.printf("Server started on port %d", port);
+            return;
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+
+        }
+        System.out.println("""
+                Chess Server:
+                java ServerMain <port>
+                """);
+
     }
 }
